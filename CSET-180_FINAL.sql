d952d947e718082dc238d0e520407a5d2b16d95f
EthanFriedrichs <ethanpfriedrichs3@gmail.com>
2024-04-22 07:39:34 -0400

    first commit

---
+++ CSET-180_FINAL.sql
@@ -0,0 +1,52 @@
+create database customers_2;
+use customers_2;
+
+-- Admin will be hard coded in
+drop table users;
+create table users (user_id int primary key auto_increment, email varchar(255), username varchar(255), proper_name varchar(255), pass varchar(255), user_type varchar(10));
+select * from users;
+update users set user_type = "Admin" where user_id = 1;
+
+drop table items;
+create table items (item_id int primary key auto_increment, item_name varchar(255), price float, in_stock int, user_id int, warranty_length int, descript varchar(255),
+foreign key (user_id) references users(user_id));
+select * from items;
+insert into items (item_name, price, in_stock, user_id, warranty_length, descript) values 
+("example item A", 1.50, 20, 2, 365, "Item A"),
+("example item B", 3.00, 12, 2, 11, "Item B"),
+("example item C", 12.75, 5, 2, 240, "Item C");
+
+drop tables describer;
+create table describer (color_id int primary key auto_increment, size varchar(255), color varchar(255), item_id int,
+foreign key (item_id) references items(item_id));
+
+drop table images;
+create table images (image_url varchar(255) primary key);
+select * from images;
+
+drop table cart;
+create table cart (cart_id int primary key auto_increment, user_id int, item_id int, quantity int,
+foreign key (user_id) references users(user_id),
+foreign key (item_id) references items(item_id));
+select * from cart;
+
+drop table complaints;
+create table complaints (complaint_id int primary key auto_increment, item_id int, time_date date not null, title varchar(20) not null, item_desc varchar(255) not null, demand varchar(14) not null, image_url varchar(255),
+foreign key (item_id) references items(item_id),
+foreign key (image_url) references images(image_url));
+select * from complaints;
+
+drop table reviews;
+create table reviews (review_id int primary key auto_increment, user_id int, review_text varchar(255) not null, accept_reject varchar(6), time_review datetime, stars float not null, image blob,
+foreign key (user_id) references users(user_id));
+select * from reviews;
+
+drop table orders;
+create table orders (order_id int primary key auto_increment, date_ordered datetime not null, item_id int not null, price int not null, user_id int, order_status varchar(10) not null,
+foreign key (user_id) references users(user_id));
+select * from orders;
+
+drop table discounts;
+create table discounts (discount_id int primary key auto_increment, discount_expire date not null, discount_percent int not null, item_id int,
+foreign key (item_id) references items(item_id));
+select * from discounts;
---
+++ main.py
@@ -0,0 +1,251 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+from sqlalchemy import create_engine, text
+from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+
+def login_required(
+    f,
+):  # redirects to login if not logged in, idk what it does otherwise
+    # Decorate routes to require login.
+
+    # http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+def admin_page(
+    f,
+):  # redirects to login if not logged in, idk what it does otherwise
+    # Decorate routes to require login.
+
+    # http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("isAdmin") == False:
+            return apology("This is admin page, u no have access", 400)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+def vendor_page(
+    f,
+):  # redirects to login if not logged in, idk what it does otherwise
+    # Decorate routes to require login.
+
+    # http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("isAdmin") == True:
+            return apology("This is user page, u no have access", 400)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+def customer_page(
+    f,
+):  # redirects to login if not logged in, idk what it does otherwise
+    # Decorate routes to require login.
+
+    # http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("isAdmin") == True:
+            return apology("This is user page, u no have access", 400)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+#configure application
+app = Flask(__name__)
+
+#configure session to use file system
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# connection string is in the format mysql://user:password@server/database
+conn_str = "mysql://root:ethanpoe125@localhost/customers_2"
+engine = create_engine(conn_str) # echo=True tells you if connection is successful or not
+db = engine.connect()
+
+
+@app.route("/")
+def main_page():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    session.clear()
+    if request.method == "POST":
+        #store all data
+        username = request.form.get("username")
+        email = request.form.get("email")
+        name = request.form.get("name")
+        password = request.form.get("password")
+        check_password = request.form.get("confirmation")
+        account_type = request.form.get("account_type")
+
+        #make sure all info exists
+        if not username or not password or not check_password or not name or not email or not account_type:
+            return apology("Missing information", 400)
+
+        #check if username already exists
+        params = {"email":email, "username":username}
+        users = db.execute(text("select * from users where email = :email"), params).all()
+        if len(users) > 0:
+            return apology("email already in use", 400)
+        
+        #check confirmation password
+        if password != check_password:
+            return apology("passwords don't match", 400)
+        
+        users = db.execute(text("select * from users where username = :username"), params).all()
+        for user in users:
+            if user[2] == username and check_password_hash(user[4], password):
+                return apology("account already in use")
+        
+
+        #generate hash and store in database
+        hashed = generate_password_hash(password)
+        params = {"username":username, "username":username, "name":name, "email":email, "hashed":hashed, "account":account_type}
+
+        
+        db.execute(text("insert into users (email, username, proper_name, pass, user_type) values (:email, :username, :name, :hashed, :account)"), params)
+        db.commit()
+        
+        return render_template("login.html")
+    
+    else:
+        return render_template("register.html")
+    
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        #store login info
+        email = request.form.get("email")
+        password = request.form.get("password")
+        
+        #check all info exists
+        if not email or not password:
+            return apology("missing information", 400)
+        
+        #call username and check password
+        params = {"email":email, "password":password}
+        users = db.execute(text("select * from users where email = :email"), params).all()
+
+        user_password = db.execute(text("select pass from users where email = :email"), params).all()
+
+        # Check if the password matches their hashed version in the database
+        if len(users) != 1 or not check_password_hash(user_password[0][0], password):
+            users = db.execute(text("select * from users where username = :email"), params).all()
+            for user in users:
+                if check_password_hash(user[4], password) and user[2] == email:
+                    session["user_id"] = user[1]
+                    session["username"] = user[2]
+                    session["loggedIn"] = True
+                    session["account_type"] = user[5]
+                    return render_template("index.html")
+            return apology("Wrong username and or password", 400)
+
+        
+        else:
+            # Sign the user in as their current username and make the session "loggedIn"
+            session["user_id"] = users[0][1]
+            session["username"] = users[0][2]
+            session["loggedIn"] = True
+            session["account_type"] = users[0][5]
+
+            return render_template("index.html")
+    else:
+        return render_template("login.html")
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    session.clear()
+    return render_template("index.html")
+
+# admin routes
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    products = db.execute(text("select * from items")).all()
+    users = db.execute(text("select user_id, username from users")).all()
+    return render_template("admin.html", products=products, users=users)
+
+@app.route("/admin/add", methods=["GET", "POST"])
+def admin_add():
+    if request.method == "POST":
+        return apology("TO DO")
+    else:
+        return apology("TO DO")
+
+@app.route("/admin/edit", methods=["GET", "POST"])
+def admin_edit():
+    if request.method == "POST":
+        return apology("TO DO")
+    else:
+        return apology("TO DO")
+
+@app.route("/admin/delete", methods=["GET", "POST"])
+def admin_delete():
+    if request.method == "POST":
+        return apology("TO DO")
+    else:
+        return apology("TO DO")
+
+
+
+# vendor routes
+@app.route("/vendor", methods=["GET", "POST"])
+
+
+@app.route("/vendor/add", methods=["GET", "POST"])
+def add_item():
+    if request.method == "POST":
+        name = request.form.get("item_name")
+        desc = request.form.get("desc")
+        price = request.form.get("item_price")
+        stock = request.form.get("curr_stock")
+        warranty = request.form.get("warranty_length")
+        color = request.form.get("color")
+        print(name, desc, price, stock, warranty, color)
+        params = {"email":session["user_id"]}
+        user = db.execute(text("select * from users where email = :email"), params).all()
+        print(user[0][0]) # User ID
+
+        return render_template("add_item.html")
+    else:
+        return render_template("add_item.html")
+
+
+def apology(message, code=400):
+    def escape(s):
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+if __name__ == '__main__':
+    app.run(debug=True) # Auto restarts cause of debug mode when changes to code are made
